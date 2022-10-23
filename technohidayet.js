6811cfd72e6d9d07b4f9167e8daa70e80b085b83
Glitch (hello-express) <none>
2022-10-23 18:46:02 +0000

    🎊👜 Checkpoint ./technohidayet.js:48891099/10893 ./package.json:48891099/2545 ./events/message.js:48891099/676 ./komutlar/küfürengel.js:48891099/3171

---
+++ technohidayet.js
@@ -0,0 +1,157 @@
+const fs=require('fs');
+const Discord=require("discord.js");
+const client=new Discord.Client();
+const db = require('quick.db')
+const moment = require("moment");
+const ayarlar=require("./ayarlar.json");
+const express = require('express');
+
+const app = express()
+app.get('/', (req, res) => res.send("Bot Aktif"))
+app.listen(process.env.PORT, () => console.log('Port ayarlandı: ' + process.env.PORT))
+
+
+
+
+client.on("message", message => {
+  let client = message.client;
+  if (message.author.bot) return;
+  if (!message.content.startsWith(ayarlar.prefix)) return;
+  let command = message.content.split(' ')[0].slice(ayarlar.prefix.length);
+  let params = message.content.split(' ').slice(1);
+  let perms = client.yetkiler(message);
+  let cmd;
+  if (client.commands.has(command)) {
+    cmd = client.commands.get(command);
+  } else if (client.aliases.has(command)) {
+    cmd = client.commands.get(client.aliases.get(command));
+  }
+  if (cmd) {
+    if (perms < cmd.conf.permLevel) return;
+    cmd.run(client, message, params, perms);
+  }
+})
+
+
+client.on("ready", () => {
+  console.log(`Bütün komutlar başarıyla yüklendi!`);
+  client.user.setStatus("dnd");
+  client.user.setActivity('Yapımcı Eren Kanks');
+})
+
+
+const log = message => {
+  console.log(`[${moment().format("YYYY-MM-DD HH:mm:ss")}] ${message}`);
+};
+
+
+client.commands = new Discord.Collection();
+client.aliases = new Discord.Collection();
+fs.readdir('./komutlar/', (err, files) => {
+  if (err) console.error(err);
+  log(`${files.length} adet komut yüklemeye hazırlanılıyor.`);
+  files.forEach(f => {
+    let props = require(`./komutlar/${f}`);
+    log(`Yüklenen komut ismi: ${props.help.name.toUpperCase()}.`);
+    client.commands.set(props.help.name, props);
+    props.conf.aliases.forEach(alias => {
+      client.aliases.set(alias, props.help.name);
+    });
+  });
+});
+client.on("message", async msg => {
+  const i = await db.fetch(`${msg.guild.id}.kufur`);
+  if (i) {
+    const kufur = [
+      "oç",
+      "amk",
+      "ananı sikiyim",
+      "ananıskm",
+      "piç",
+      "amk",
+      "amsk",
+      "sikim",
+      "sikiyim",
+      "orospu çocuğu",
+      "piç kurusu",
+      "kahpe",
+      "orospu",
+      "mal",
+      "sik",
+      "yarrak",
+      "am",
+      "amcık",
+      "amık",
+      "yarram",
+      "sikimi ye",
+      "mk",
+      "mq",
+      "aq",
+      "ak",
+      "amq"
+    ];
+    if (kufur.some(word => msg.content.includes(word))) {
+      try {
+        if (!msg.member.permissions.has("BAN_MEMBERS")) {
+          msg.delete();
+
+          return msg
+            .reply("Heey! Küfür Yasak.")
+            .then(wiskyx => wiskyx.delete({ timeout: 5000 }));
+        }
+      } catch (err) {
+        console.log(err);
+      }
+    }
+  }
+  if (!i) return;
+});
+
+client.on("messageUpdate", async msg => {
+  const i = db.fetch(`${msg.guild.id}.kufur`);
+  if (i) {
+    const kufur = [
+      "oç",
+      "amk",
+      "ananı sikiyim",
+      "ananıskm",
+      "piç",
+      "amk",
+      "amsk",
+      "sikim",
+      "sikiyim",
+      "orospu çocuğu",
+      "piç kurusu",
+      "kahpe",
+      "orospu",
+      "mal",
+      "sik",
+      "yarrak",
+      "am",
+      "amcık",
+      "amık",
+      "yarram",
+      "sikimi ye",
+      "mk",
+      "mq",
+      "aq",
+      "ak",
+      "amq"
+    ];
+    if (kufur.some(word => msg.content.includes(word))) {
+      try {
+        if (!msg.member.permissions.has("BAN_MEMBERS")) {
+          msg.delete();
+
+          return msg
+            .reply("Yakaladım Seni! Küfür Yasak.")
+            .then(wiskyx => wiskyx.delete({ timeout: 5000 }));
+        }
+      } catch (err) {
+        console.log(err);
+      }
+    }
+  }
+  if (!i) return;
+});
+
